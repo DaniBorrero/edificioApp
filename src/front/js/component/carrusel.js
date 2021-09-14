abf65db470e7b42cdd91a2b3277f30e161d019f2
DaniBorrero <borrerodaniel89@gmail.com>
2021-09-14 23:42:44 +0000

    test

--- src/front/js/component/carrusel.js
+++ src/front/js/component/carrusel.js
@@ -12,7 +12,7 @@ export const Carrusel = () => {
 						alt="First slide"
 					/>
 					<Carousel.Caption>
-						<h3>First slide label</h3>
+						<h3>Fachada</h3>
 						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
 					</Carousel.Caption>
 				</Carousel.Item>
