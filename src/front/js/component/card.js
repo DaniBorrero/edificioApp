1444fea7872c57ac2cec82c8c0610b4ee443c1e2
DaniBorrero <borrerodaniel89@gmail.com>
2021-09-16 02:12:25 +0000

    cambiando a React Bootstrap navbar y card

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -1,22 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
 
-export const Card = () => {
+export const Cards = () => {
 	return (
 		<>
-			<div className="card" style={{ width: "18rem" }}>
-				<img
+			<Card className="bg-dark text-white mx-3">
+				<Card.Text className="text-center	">Pedro Perez - Conserje</Card.Text>
+				<Card.Img
 					src="https://s3-sa-east-1.amazonaws.com/media.reistock.com/wp-content/uploads-mkt-wp/sites/12/2018/11/19171148/Conserje.png"
-					className="card-img-top"
-					alt="..."
+					alt="Card image"
 				/>
-				<div className="card-body">
-					<p className="card-text">
-						Some quick example text to build on the card title and make up the bulk of the card&apos;s
-						content.
-					</p>
-				</div>
-			</div>
+			</Card>
 		</>
 	);
 };
