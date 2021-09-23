22d376e8ca8275ca4284c8126e4fe03eab5b0841
pedroamenga <pedroam92@gmail.com>
2021-09-23 00:43:51 +0000

    avanceacordeonlogged

--- src/front/js/pages/logged.js
+++ src/front/js/pages/logged.js
@@ -1,1 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Acordeonlogged } from "../component/acordeonlogged";
+import { Container, Row } from "react-bootstrap";
+import "../../styles/registry.scss";
+
+export const LoggedScreen = () => {
+	return (
+		<>
+			<Container className="loggedContainer">
+				<Row>
+					<Acordeonlogged />
+				</Row>
+			</Container>
+			;
+		</>
+	);
+};
