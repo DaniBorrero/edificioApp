3c9a3a0bef15fa8310b5141dd9b4ba9283e34da6
DaniBorrero <borrerodaniel89@gmail.com>
2021-10-02 06:34:07 +0000

    login funcionando en front

--- src/front/js/pages/registry.js
+++ src/front/js/pages/registry.js
@@ -9,20 +9,6 @@ export const RegistryForm = () => {
 		<Container className="registryConteiner">
 			<Row>
 				<Col className="registryLeft">
-					<Form className="registryForm">
-						<Form.Group controlId="formBasicEmail">
-							<Form.Label>Email address</Form.Label>
-							<Form.Control type="email" placeholder="Enter email" />
-						</Form.Group>
-
-						<Form.Group controlId="formBasicPassword">
-							<Form.Label>Password</Form.Label>
-							<Form.Control type="password" placeholder="Password" />
-						</Form.Group>
-						<Button variant="primary" type="submit">
-							Ingresar
-						</Button>
-					</Form>
 					<AccordionForm />
 				</Col>
 				<Col className="registryRight">
