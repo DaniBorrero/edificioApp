e23665da58c7c6ce1995a284a1ea0c0fdb22c989
pedroamenga <pedroam92@gmail.com>
2021-10-21 00:44:16 +0000

    teams

---
+++ src/front/js/component/equiposlanding.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const equiposLanding = () => {
+    return (
+        <>
+            <div styles={{ height: "600px" }}>
+                <Figure>
+                    <Figure.Image
+                        width={171}
+                        height={180}
+                        alt="171x180"
+                        src="../../img/Perfil1.jpg"
+                    />
+                    <Figure.Caption>
+                        Nulla vitae elit libero, a pharetra augue mollis interdum.
+                    </Figure.Caption>
+                </Figure>
+
+            </div>
+
+
+        </>
+    )
+}
