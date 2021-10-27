d988fa1011c8ac495b8612aeaf4442a32c02e3f9
DaniBorrero <borrerodaniel89@gmail.com>
2021-10-27 13:48:45 +0000

    validando formularios que pase por alto

--- src/front/js/component/acordeonlogged.js
+++ src/front/js/component/acordeonlogged.js
@@ -62,9 +62,10 @@ export const Acordeonlogged = () => {
 										<Form.Control
 											type="text"
 											placeholder="Coloque el titulo del anuncio"
+											required
 											onChange={e => setTitulo(e.target.value)}
 											value={Titulo}
-											required
+											
 										/>
 									</Form.Group>
 									<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
@@ -72,6 +73,7 @@ export const Acordeonlogged = () => {
 										<Form.Control
 											type="text"
 											placeholder="Coloque el tipo publicacion"
+											required
 											onChange={e => setPublicacion(e.target.value)}
 											value={TipoPublicacion}
 										/>
@@ -81,6 +83,7 @@ export const Acordeonlogged = () => {
 										<Form.Control
 											type="text"
 											placeholder="Coloque el anuncio a mostrar en el diario mural"
+											required
 											onChange={e => setAnuncio(e.target.value)}
 											value={Anuncio}
 										/>
